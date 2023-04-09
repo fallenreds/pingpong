de61571827ef85d1a7707c42b05a6f8ee39575a1
Artur <97253965+fallenreds@users.noreply.github.com>
2023-04-09 19:05:56 +0300

    class GameSprite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+
 pygame.init()
 
 background_color = (0, 155, 255)
@@ -8,12 +9,31 @@ window.fill(background_color)
 clock = pygame.time.Clock()
 
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__(self, img, x,y, width, height):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(img), (width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
 
-while True:
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+a = GameSprite("imgs/ball.png", 0,0, 100, 100)
+
+
+game = True
+while game:
+
     window.fill(background_color)
+    a.reset()
+    # window.blit(ball, (0,0))
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             game = False
+    keys = pygame.key.get_pressed()
+
 
     pygame.display.update()
     clock.tick(60)
